db52487d83de2217e240696490024262a8f5645c
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-10-17 12:28:21 +0530

    first commit

--- src/routes/App.Router.jsx
+++ src/routes/App.Router.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Work from "../pages/Work";
 import Skill from "../pages/Skill";
-import NotFound from "../pages/NotFound";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -19,7 +18,7 @@ const AppRouter = () => {
          <Route path="/contact" element={<Contact />} />
          <Route path="/project" element={<Work />} />
          <Route path="/skill" element={<Skill />} />
-         <Route path="*" element={<Navigate to='/' />} />
+         <Route path="*" element={<Home />} />
        </Routes>
      <Footer/>
      </Router>
